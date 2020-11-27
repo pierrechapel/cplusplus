8b1cea34efb00081f53d259341be731d5e69ff68
Valérie Roy <valerie.roy@mines-paristech.fr>
2020-11-27 18:20:42 +0100

    commentaires sur votre problèmes

--- int_stack/test_intstack.cpp
+++ int_stack/test_intstack.cpp
@@ -2,11 +2,22 @@
 #include "int_stack.h"
 
 int main(){
+  // vr: mettez les tests dans une fonction de test et appelez la
+  // fonction dans votre main, comme cela vous pourrez ré-utiliser
+  // cette fonction dans d'autres occasionsq
     try{
     int_stack stack(-5);
     } catch(const char* msg){
         std::cerr<<msg<<'\n';
     }
+
+    // vr: le bloc dans lequel vous avez défini la variable stack est
+    // fermé et de toutes les manières vous ne pourriez pas
+    // ré-utiliser la pile stack puisqu'elle a une taille négative
+
+    // Ainsi, si vous voulez continuer à tester votre code vous devez,
+    // dans chaque try, redéfinir une variable stack lui appliquer
+    // d'autres tests
 
     try{
         stack.pop();
